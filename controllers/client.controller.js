d9322bcd0cefb88b652ea70aef76a23e85421373
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-08-06 02:58:39 +0100

    update the file upload ro have the duplicate on pc

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -12,18 +12,19 @@ exports.uploadFile = async (req, res) => {
         return res.status(400).json({ status: false, message: 'Description is required', data: null });
     }
     // Create a temporary file path
-    // const tempFilePath = path.join(__dirname, req.file.originalname);
+    const tempFilePath = path.join(__dirname, req.file.originalname);
     // Write the buffer contents to the temporary file
-    // await fs.writeFile(tempFilePath, req.file.buffer);
+    await fs.writeFile(tempFilePath, req.file.buffer);
+    const result = await cloudinary.uploader.upload(tempFilePath, { resource_type: 'auto' })
 
     // Use the path of the uploaded file on disk for Cloudinary upload
-    const result = await cloudinary.uploader.upload(req.file.path, { resource_type: 'auto' })
+    //const result = await cloudinary.uploader.upload(req.file.path, { resource_type: 'auto' })
     // Delete the uploaded file from disk after Cloudinary upload
-    fs.unlink(req.file.path, (err) => {
-        if (err) {
-            console.error("Error deleting file from disk:", err);
-        }
-    });
+    // fs.unlink(req.file.path, (err) => {
+    //     if (err) {
+    //         console.error("Error deleting file from disk:", err);
+    //     }
+    // });
 
     const payload = { file_url: result?.secure_url, public_id: result.public_id, description: req.body.description }
     const newFile = new XmlFile(payload);
--- routes/client.route.js
+++ routes/client.route.js
@@ -4,7 +4,8 @@ const { storage } = require("../util/cloudinary");
 const multer = require('multer');
 const router = express.Router();
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/upload", upload.single('file'), controller.uploadFile);
 router.get("/retrieve", controller.getFiles);
